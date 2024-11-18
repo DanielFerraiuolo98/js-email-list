f79482d0d6d2a1356182319d86a1033d498c0f7b
DanielFerraiuolo98 <daniel.ferraiuolo.98@gmail.com>
2024-11-18 17:02:54 +0100

    aggiunta del bottone per stampare le mail

--- script/script.js
+++ script/script.js
@@ -2,25 +2,26 @@
 
 //https://flynn.boolean.careers/exercises/api/random/mail
 
-const email = document.getElementById("emailList")
-let mails = [];
+const email = document.getElementById("emailList");
+const btn = document.querySelector(".btn-secondary");
 
-for (let i = 0; i < 10; i++) {
-    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((dati) => {
-        mails.push(dati.data.response);
-        if (mails.length === 10) {
-            printEmail(mails, email);
-        }
-    })
-}
+btn.addEventListener("click", function () {
+    let mails = [];
+    for (let i = 0; i < 10; i++) {
+        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((dati) => {
+            mails.push(dati.data.response);
+            if (mails.length === 10) {
+                printEmail(mails, email);
+            }
+        });
+    }
+});
 
 function printEmail(array, element) {
     console.log(array);
-
     let template = "";
     array.forEach(email => {
-        template += `<li>${email}</li>`
-
+        template += `<li>${email}</li>`;
     });
     element.innerHTML = template;
 }
